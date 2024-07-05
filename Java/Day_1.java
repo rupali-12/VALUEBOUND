bbd1df2077eb320973607452f9e1a2b8e6dc2b8d
rupali-12 <rupalisharma1009@gmail.com>
2024-07-05 10:52:31 +0530

    first commit

---
+++ Java/Day_1.java
@@ -0,0 +1,24 @@
+
+// // ****************SECTION_2 ********************************* 
+
+// import java.util.Scanner;
+
+// public class Day_1 {
+//     public static void main(String[] args) {
+//         // System.out.println("mdkmc");
+//         // int a = 10;
+//         // System.out.println(a >> 2);
+//         // System.out.println(a << 2);
+
+//         float weight, height;
+//         Scanner sc = new Scanner(System.in);
+//         weight = sc.nextFloat();
+//         height = sc.nextFloat();
+
+//         float bmi = weight / (height * height);
+//         System.out.println(bmi);
+//     }
+// }
+
+
+// ************************SECTION_4 ************************************
